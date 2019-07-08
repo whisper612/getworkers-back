61d4ad7a08acbb0dae9a5bef12fa0cd1222ad9c8
PavelChubakov <mr.waylander@bk.ru>
2019-07-08 19:32:10 +0700

    -v server 0.8.0: bot integration in progress

--- api/admin/admin_authentication.js
+++ api/admin/admin_authentication.js
@@ -5,7 +5,7 @@ module.exports = function(app, pool, tokenObject) {
         if (token_client === tokenObject.TOKEN){
             console.log('Admin authentication acquired');
 
-            pool.query('SELECT * FROM orders', (err, data) => {
+            pool.query('SELECT * FROM orders ORDER BY `create_time` DESC LIMIT 1000;', (err, data) => {
                 (err)?res.send(err):res.json({orders: data});
             });
         } else {
