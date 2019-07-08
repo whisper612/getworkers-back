ec03fbecc752583ac6dc2b84e970637fbd078dfb
PavelChubakov <mr.waylander@bk.ru>
2019-07-08 19:06:26 +0700

    -v server 0.8.0: bot integration in progress

--- api/bot/bot.js
+++ api/bot/bot.js
@@ -2,11 +2,16 @@ module.exports = function(app, bot, pool) {
     bot.start((ctx) => {
         console.log('Bot has been deployed together with a server')
     });
-    bot.command('register', (ctx) => ctx.reply('Try send a sticker'));
+    const userId = null;
+    bot.command('register', (ctx) => 
+        userId = telegram.getChatMember(ctx, ctx)
+    );
     
-    bot.hears('hi', (ctx) => ctx.reply('Hello, stranger!'));
-    bot.hears(/bye/i, (ctx) => ctx.reply('Bye-bye, stranger!'));
-    bot.on('sticker', (ctx) => ctx.reply('👍'))
+
+
+    // bot.hears('hi', (ctx) => ctx.reply('Hello, stranger!'));
+    // bot.hears(/bye/i, (ctx) => ctx.reply('Bye-bye, stranger!'));
+    // bot.on('sticker', (ctx) => ctx.reply('👍'))
     
     bot.launch()
 }
