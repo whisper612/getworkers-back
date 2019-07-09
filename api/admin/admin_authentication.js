a2cd77571d7e71ded10ccb525cd791766f8f947a
PavelChubakov <mr.waylander@bk.ru>
2019-07-09 12:33:51 +0700

    -v 0.8.2 server: admin auth fix

--- api/admin/admin_authentication.js
+++ api/admin/admin_authentication.js
@@ -5,9 +5,10 @@ module.exports = function(app, pool, tokenObject) {
         if (token_client === tokenObject.TOKEN){
             console.log('Admin authentication acquired');
 
-            pool.query('SELECT * FROM orders ORDER BY `create_time` DESC LIMIT 1000; SELECT * FROM executors_list ORDER BY `create_time` DESC LIMIT 1000;', 
-                (err, data, executors) => {
-                    (err)?res.send(err):console.log( res.json({orders: ordersData, executors_list: executorsData }) );
+            const query = 'SELECT * FROM orders ORDER BY `create_time` DESC LIMIT 1000; SELECT * FROM executors_list;';
+            
+            pool.query(query, (err, data) => {
+                    (err)?res.send(err):res.json({orders: data[0], executors_list: data[1]});
             });
             
         } else {
--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const PORT = (process.env.PORT || 5000);
 
 // Database connect
 var pool = mysql.createPool({
+    multipleStatements: true,
     connectionLimit: tokenObject.connectionLimit,
     host: tokenObject.host,
     user: tokenObject.user,
