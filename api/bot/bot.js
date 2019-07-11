651574e716a19143ee0164074ed09ac6d0a7261c
PavelChubakov <mr.waylander@bk.ru>
2019-07-11 16:17:11 +0700

    Bot developing test

--- api/bot/bot.js
+++ api/bot/bot.js
@@ -32,14 +32,13 @@ module.exports = function(app, bot, telegramObject, telegrafObject, pool) {
 
 	bot.on('contact', (ctx, pool) => {
 	if (ctx.update.message.contact !== undefined) {
-		ctx.reply('Успешно! Для завершения регистрации, пожалуйста, укажите своё имя.', (ctx) => {
-			let local = ctx.update.message.text;
-			console.log(local)
+		ctx.reply('Успешно! Для завершения регистрации, пожалуйста, укажите своё имя.',
+			console.log(ctx.update.message.text),
 			bot.hears(local, (ctx) => {
 				console.log(ctx.update.message.contact)
 				ctx.reply('Регистрация успешно завершена, если вы ошибилсь при написании имени, то сообщите об этом администратору.')
 			})
-		})
+		)
 	}  else {
 			return ctx.reply('Что-то пошло не так и я не получил ваш номер телефона. Попробуйте ещё раз.', Markup
 				.keyboard([
