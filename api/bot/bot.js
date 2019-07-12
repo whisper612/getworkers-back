cadd8cd550f99759e5b8482bdd77fc1195fa8584
PavelChubakov <mr.waylander@bk.ru>
2019-07-12 11:58:22 +0700

    Bot developing

--- api/bot/bot.js
+++ api/bot/bot.js
@@ -15,7 +15,7 @@ module.exports = function(bot, telegramApi) {
 			console.log('New developer has been spotted!')
 			return ctx.reply('Дорогой разраб, твои логи отправлены в серверную консоль',
 			console.log(ctx.update),
-			telegrafObject.sendMessage('-374124420', 'Test request', (ctx) => {
+			telegramApi.sendMessage('-374124420', 'Test request', (ctx) => {
 				console.log(ctx.update)
 			})
 			)	
