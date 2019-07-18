54fd054cf108a1ab552a08ca5a3fd77bf944d365
PavelChubakov <mr.waylander@bk.ru>
2019-07-18 12:20:54 +0700

    Bot developing

--- api/database/db_requests.js
+++ api/database/db_requests.js
@@ -80,7 +80,7 @@ module.exports = function(app, pool, telegramApi, tokenObject) {
             const telegramMsg = `🗺️ <b>Куда:</b> <i>${address}</i>\n\n⏰ <b>Когда:</b> <i>${meeting_date_time}</i>\n\n👷 <b>Работников нужно:</b> ${executors_count}
             \n🗒️ <b>Задание:</b> <i>${description}</i>\n\n💵 <b>Стоимость заказа:</b> ${price * 0.8}<b>₽</b>`
 
-            inlineButton = InlineKeyboardMarkup(text = `🚚 Приступить к работе`)
+            inlineButton = InlineKeyboardMarkup(text = `🚚 Приступить к работе`, callback_data = `Ашотик нажал кнопку`)
 
             const query = 
             `UPDATE orders SET phone = ?, name = ?, address = ?, description = ?, price = ?,
