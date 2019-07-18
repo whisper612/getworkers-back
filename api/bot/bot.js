8cd6860ae3db97a58ff0e83ae4514087b1e1e2d4
PavelChubakov <mr.waylander@bk.ru>
2019-07-18 16:55:55 +0700

    Bot developing

--- api/bot/bot.js
+++ api/bot/bot.js
@@ -68,6 +68,7 @@ module.exports = function(bot, telegramApi, tokenObject) {
 		})
 		.then(res => {
 			console.log(res)
+			console.log(JSON.parse(res.data.check))
 			if (res.data.check.orderId !== null) {
 				return telegramApi.sendMessage(executorId, `Вы уже взяли заказ под номером ${res.data.check.orderId}`)
 			} else {
