3d960c1a92cdcd3540ae73e99de7b2f067fd0286
PavelChubakov <mr.waylander@bk.ru>
2019-07-18 16:22:42 +0700

    Bot developing

--- api/database/db_requests.js
+++ api/database/db_requests.js
@@ -233,7 +233,7 @@ module.exports = function(app, pool, telegramApi, tokenObject) {
                         console.log(err, `Error: /select_executor: affected rows ${result.affectedRows} < 1`)
                         res.send(err)
                     } else {
-                        console.log(JSON.stringify(result[0].@order_id))
+                        console.log(JSON.stringify(result[0]))
                         res.send({check: 'result.RowDataPacket.order_id'})
                     }
                 }
