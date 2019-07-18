279d8832b27bf490fc874cc872a6664f3e32c9dd
PavelChubakov <mr.waylander@bk.ru>
2019-07-18 18:01:16 +0700

    Bot developing

--- api/bot/bot.js
+++ api/bot/bot.js
@@ -77,7 +77,7 @@ module.exports = function(bot, telegramApi, tokenObject) {
 				})
 				.then(res => {
 					console.log(res)
-					const reply = `<b>Вы первым откликнулись на заказ!</b>\n\nТеперь вам нужно:\n<i>Дождаться оставшихся работников\n\nCобраться вместе и отправиться к <i>заказчику</i>.\n\n`
+					const reply = `<b>Вы первым откликнулись на заказ!</b>\n\nТеперь вам нужно:\n<i>Дождаться оставшихся работников\n\nCобраться вместе и отправиться к <i>заказчику</i>.`
 					const extra = {parse_mode: `HTML`}
 					console.log(executorId)
 					return telegramApi.sendMessage(executorId, reply, extra)
