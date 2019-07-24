c67800e88365a04c2fb481dd0ba30b76cfd1cf28
PavelChubakov <mr.waylander@bk.ru>
2019-07-24 22:43:56 +0700

    Some code refactoring

--- api/bot/bot.js
+++ api/bot/bot.js
@@ -150,7 +150,6 @@ module.exports = function(bot, telegramApi, tokenObject) {
 								👷 <b>Имя:</b> ${name}
 								📞 <b>Телефон:</b> ${phone}`;
 
-								console.log('AAAAAAAAAAAAAAAAAAAAAAAA БЛЯ',executorId, reply)
 								return telegramApi.sendMessage(executorId, reply, {parse_mode: `HTML`})
 							})
 						})
